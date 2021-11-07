07f8fccf7e60c4e5426b0d2333a1892913a11231
DariuszMeissner <dariusz.r.meissner@gmail.com>
2021-11-07 15:54:28 +0100

    added composeWithDevTools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
 
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)))
 
 export default store
